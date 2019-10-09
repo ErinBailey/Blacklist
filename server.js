c7fe7ac9aa5c18ae93115d69a26c0d46e86668e3
Erin <emb255@nau.edu>
2019-10-09 01:51:56 -0500

    logs

--- server.js
+++ server.js
@@ -1,21 +1,11 @@
 const express = require("express");
+const bodyParser = require("body-parser")
 const { join } = require("path");
 const app = express();
-// var createHandler = require('github-webhook-handler')
-// var handler = createHandler({ path: '/webhook', secret: 'secret123321'});
+
+var jsonParser = bodyParser.json()
 
 app.use(express.static(join(__dirname, "public")));
-// app.use(createHandler);
-
-// handler.on('error', function (err) {
-//   console.error('Error:', err.message)
-// })
-
-// handler.on('push', function (event) {
-//   console.log('Received a push event for %s to %s',
-//     event.payload.repository.name,
-//     event.payload.ref)
-// })
 
 app.get("/auth_config.json", (req, res) => {
   res.sendFile(join(__dirname, "auth_config.json"));
@@ -25,15 +15,17 @@ app.get("/", (_, res) => {
   res.sendFile(join(__dirname, "index.html"));
 });
 
-app.post("/sample-ip-event", (req, res) => {
+app.post("/sample-ip-event", jsonParser, (req, res) => {
   console.log("req.connection.remoteAddress!!!!!", req.connection.remoteAddress) // remote IP
   console.log("request.headers['x-forwarded-for']!!!!!", req.headers['x-forwarded-for']) // remote IP if the server is behind a proxy
-  var message = req
   var headers = req.headers
   console.log("headers!!!!", headers)
-  console.log("message!!!!", message)
+  console.log("request!!!!!", req)
+  console.log("requestBody!!!!!", req.body)
+  console.log("response!!!!!", res)
+  console.log("responseBody!!!!!", res.body)
   res.status(200);
-  res.send(req.body);
+  res.send(res.body);
 });
 
 const port =  process.env.PORT || 3000;
