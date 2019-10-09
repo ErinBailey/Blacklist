de6ecacebb3f26d282182b7d734613d97dcded3b
Erin <emb255@nau.edu>
2019-10-08 23:52:46 -0500

    test webhook handler

--- server.js
+++ server.js
@@ -3,17 +3,28 @@ const { join } = require("path");
 const morgan = require("morgan");
 const helmet = require("helmet");
 const app = express();
+var bodyParser = require('body-parser')
 
 app.use(morgan("dev"));
 app.use(helmet());
 app.use(express.static(join(__dirname, "public")));
+app.use(bodyParser.json());
 
 app.get("/auth_config.json", (req, res) => {
   res.sendFile(join(__dirname, "auth_config.json"));
 });
 
-app.get("/*", (_, res) => {
+app.get("/", (_, res) => {
   res.sendFile(join(__dirname, "index.html"));
+});
+
+app.post("/sample-ip-event", (req, res) => {
+  console.log("req.connection.remoteAddress!!!!!", req.connection.remoteAddress) // remote IP
+  console.log("request.headers['x-forwarded-for']!!!!!", req.headers['x-forwarded-for']) // remote IP if the server is behind a proxy
+  var message = req.body
+  console.log("message test!!!!", message)
+  res.status(200);
+  res.send(req.body);
 });
 
 process.on("SIGINT", function() {
